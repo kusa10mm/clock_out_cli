cce29c20f805d97652d134b021ab2b4ee7a3686c
草野浩俊 <kfactor.95.t@gmail.com>
2021-02-03 08:52:57 +0900

    divide calc_actions

---
+++ src/calc_actions.rs
@@ -0,0 +1,48 @@
+use crate::status_enum::*;
+use crate::clock_out_status::*;
+use chrono::prelude::*;
+
+pub fn calc_actions(status: ClockOutStatus) -> Vec<String> {
+    let dinner_options = calc_dinner(status.stress, status.time);
+    let do_laundry = calc_do_laundry(status.laundry_amount);
+    let actions = calc_order_of_actions(dinner_options, do_laundry, status.hunger);
+    actions
+}
+
+fn calc_order_of_actions(dinner_options: Dinner, do_laundry: bool, hunger: Hunger) -> Vec<String> {
+    let mut actions = Vec::<String>::new();
+    if do_laundry {
+        actions.push(String::from("Do Laundry"));
+    };
+    if hunger == Hunger::ALittle {
+        actions.push(String::from("Training"));
+        actions.push(String::from("Dinner"));
+    } else {
+        actions.push(String::from("Dinner"));
+        actions.push(String::from("Training"));
+    }
+    actions
+}
+
+fn calc_do_laundry(laundry_amount: f32) -> bool {
+    if laundry_amount < 1.5 {
+        false
+    } else {
+        true
+    }
+}
+
+fn calc_dinner(stress: Stress, time: DateTime<Local>) -> Dinner {
+    let dinner = match stress {
+        Stress::High => match time.hour() {
+            0..=19 => Dinner::Rice,
+            _ => Dinner::NabeSet,
+        },
+        Stress::Low => match time.hour() {
+            0..=19 => Dinner::Rice,
+            _ => Dinner::Convinience
+        }
+    };
+    println!("Dinner Options: {:?}", dinner);
+    dinner
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod clock_out_status;
 mod status_enum;
+mod calc_actions;
 
 use std::env;
-use chrono::prelude::*;
-use status_enum::*;
 use clock_out_status::*;
+use calc_actions::*;
 
 fn main() {
     // 引数を受け取る
@@ -22,50 +22,4 @@ fn main() {
     };
 }
 
-
-
-fn calc_actions(status: ClockOutStatus) -> Vec<String> {
-    let dinner_options = calc_dinner(status.stress, status.time);
-    let do_laundry = calc_do_laundry(status.laundry_amount);
-    let actions = calc_order_of_actions(dinner_options, do_laundry, status.hunger);
-    actions
-}
-
-fn calc_order_of_actions(dinner_options: Dinner, do_laundry: bool, hunger: Hunger) -> Vec<String> {
-    let mut actions = Vec::<String>::new();
-    if do_laundry {
-        actions.push(String::from("Do Laundry"));
-    };
-    if hunger == Hunger::ALittle {
-        actions.push(String::from("Training"));
-        actions.push(String::from("Dinner"));
-    } else {
-        actions.push(String::from("Dinner"));
-        actions.push(String::from("Training"));
-    }
-    actions
-}
-
-fn calc_do_laundry(laundry_amount: f32) -> bool {
-    if laundry_amount < 1.5 {
-        false
-    } else {
-        true
-    }
-}
-
-fn calc_dinner(stress: Stress, time: DateTime<Local>) -> Dinner {
-    let dinner = match stress {
-        Stress::High => match time.hour() {
-            0..=19 => Dinner::Rice,
-            _ => Dinner::NabeSet,
-        },
-        Stress::Low => match time.hour() {
-            0..=19 => Dinner::Rice,
-            _ => Dinner::Convinience
-        }
-    };
-    println!("Dinner Options: {:?}", dinner);
-    dinner
-}
 
